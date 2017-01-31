29f46ff4954ab77e92ab8f8f811ea9804f936988
Mike Pastore <mike@oobak.org>
2017-01-31 02:23:53 -0600

    Replace Docker pull on start with update script

---
+++ roles/docker/files/docker-update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -p /run/docker-update
+psout=$(mktemp)
+function cleanup {
+  rmdir /run/docker-update 2>/dev/null
+  rm -f $psout
+}
+trap cleanup EXIT
+
+echo "Querying Docker..."
+docker ps --filter 'Name=\.service' --format '{{.Image}}\t{{.Names}}' >$psout
+images=$(awk '{ print $1 }' $psout | sort -u)
+echo "Found $(echo $(wc -w <<< $images)) image(s) and $(echo $(wc -l < $psout)) service(s)!"
+
+for image in $images ; do
+  if docker pull $image | grep -qs "Image is up to date" ; then
+    echo "Image $image is up to date."
+  else
+    echo "Image $image has been updated."
+    for name in $(awk -v image=$image '$1 == image { print $2 }' $psout) ; do
+      touch /run/docker-update/${name}.updated
+    done
+  fi
+done
+
+shopt -s nullglob
+notices=(/run/docker-update/*.updated)
+shopt -u nullglob
+
+for notice in ${notices[*]+"${notices[*]}"} ; do
+  name=$(basename $notice .updated)
+
+  echo "Restarting ${name}..."
+  systemctl restart $name
+
+  rm -f $notice
+done
+
+echo "Cleaning up unused containers and dangling images..."
+docker system prune -af
+
+echo "Done!"
